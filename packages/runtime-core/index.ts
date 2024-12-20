73efb24d46d46b8680020ebce2e8fd8290b03f52
Мухин Григорий <muhin.g.s.00@gmail.com>
2024-12-20 19:10:35 +0300

    refactoring: separated the rendering logic from the platform-dependent implementation and the base API

---
+++ packages/runtime-core/index.ts
@@ -0,0 +1,5 @@
+export type { RendererOptions } from './renderer'
+export { createRenderer } from './renderer'
+
+export type { App, CreateAppFunction } from './apiCreateApp'
+export { createAppAPI } from './apiCreateApp'

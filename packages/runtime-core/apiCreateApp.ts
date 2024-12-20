c15f914c3b8130a9e102707f868bacab8f19d903
Мухин Григорий <muhin.g.s.00@gmail.com>
2024-12-20 20:57:24 +0300

    feat: add simple vdom

--- packages/runtime-core/apiCreateApp.ts
+++ packages/runtime-core/apiCreateApp.ts
@@ -1,4 +1,3 @@
-import { ReactiveEffect } from '../reactivity'
 import { Component } from './component'
 import { RootRenderFunction } from './renderer'
 
@@ -14,19 +13,11 @@ export function createAppAPI<HostElement>(
   render: RootRenderFunction<HostElement>,
 ): CreateAppFunction<HostElement> {
   return function createApp(rootComponent) {
-		const app: App = {
-			mount(rootContainer: HostElement) {
-				const componentRender = rootComponent.setup!()
-		
-				const updateComponent = () => {
-					const vnode = componentRender()
-					render(vnode, rootContainer)
-				}
-		
-				const effect = new ReactiveEffect(updateComponent)
-        effect.run()
-			},
-		}
+    const app: App = {
+      mount(rootContainer: HostElement) {
+        render(rootComponent, rootContainer)
+      },
+    }
 
     return app
   }

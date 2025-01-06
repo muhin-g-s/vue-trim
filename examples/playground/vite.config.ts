8ea45a4077a3ad0b2fdd70dfa25a7d0304965e6d
Мухин Григорий <muhin.g.s.00@gmail.com>
2025-01-06 22:51:23 +0300

    create simple sfc parser

--- examples/playground/vite.config.ts
+++ examples/playground/vite.config.ts
@@ -2,11 +2,15 @@ import path from 'node:path'
 import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
 
+import VueTrimPlugin from '../../packages/@extensions/vite-plugin-vue-trim';
+
 const dirname = path.dirname(fileURLToPath(new URL(import.meta.url)))
+
 export default defineConfig({
   resolve: {
     alias: {
-			'vue-trim': path.resolve(dirname, '../../packages'),
+      'vue-trim': path.resolve(dirname, '../../packages'),
     },
   },
+  plugins: [VueTrimPlugin()],
 })
---
+++ packages/@extensions/vite-plugin-vue-trim/index.ts
@@ -0,0 +1,20 @@
+import { createFilter, type Plugin } from 'vite'
+import { parse } from '../../compiler-sfc'
+
+export default function vitePluginChibivue(): Plugin {
+  const filter = createFilter(/\.vue$/)
+
+  return {
+    name: 'vite:chibivue',
+
+    transform(code, id) {
+      if (!filter(id)) return
+      const { descriptor } = parse(code, { filename: id })
+      console.log(
+        '🚀 ~ file: index.ts:14 ~ transform ~ descriptor:',
+        descriptor,
+      )
+      return { code: `export default {}` }
+    },
+  }
+}

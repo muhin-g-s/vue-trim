b943334c52a913e9771823cf7e60891298cdf1bf
Мухин Григорий <muhin.g.s.00@gmail.com>
2025-01-08 18:53:07 +0300

    add import triggerRef

--- examples/playground/src/main.ts
+++ examples/playground/src/main.ts
@@ -1,8 +1,11 @@
-import { createApp, h, shallowRef } from 'vue-trim'
+import { createApp, h, shallowRef, triggerRef } from 'vue-trim'
 
 const app = createApp({
   setup() {
     const state = shallowRef({ count: 0 })
+    const forceUpdate = () => {
+      triggerRef(state)
+    }
 
     return () =>
       h('div', {}, [
@@ -26,6 +29,12 @@ const app = createApp({
             },
           },
           ['not trigger ...'],
+        ),
+
+        h(
+          'button', // The rendering is updated to the value currently held by state.value.count
+          { onClick: forceUpdate },
+          ['force update !'],
         ),
       ])
   },

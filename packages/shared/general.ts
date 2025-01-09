0af6cd5195e14a100eb6808db0fb28297ce58802
Мухин Григорий <muhin.g.s.00@gmail.com>
2025-01-09 04:20:50 +0300

    add computed setter

--- packages/shared/general.ts
+++ packages/shared/general.ts
@@ -1,3 +1,5 @@
+export const isFunction = (val: unknown): val is Function =>
+  typeof val === 'function'
 export const isArray = Array.isArray
 export const isString = (val: unknown): val is string => typeof val === 'string'
 export const isObject = (val: unknown): val is Record<any, any> =>

59b77fbc7d8b38b2cd4ece09781710e66a041a23
Мухин Григорий <muhin.g.s.00@gmail.com>
2025-01-09 05:25:26 +0300

    add parameters in watch and multi source

--- packages/runtime-core/apiWatch.ts
+++ packages/runtime-core/apiWatch.ts
@@ -1,26 +1,95 @@
-import { ReactiveEffect } from '../reactivity'
-import { hasChanged } from '../shared'
+import { ComputedRef, ReactiveEffect, Ref, isRef } from '../reactivity'
+import {
+  hasChanged,
+  isArray,
+  isFunction,
+  isMap,
+  isObject,
+  isPlainObject,
+  isSet,
+} from '../shared'
 
-export type WatchEffect = (onCleanup: OnCleanup) => void
+export type WatchSource<T = any> = (() => T) | Ref<T> | ComputedRef<T> | object
 
-export type WatchSource<T = any> = () => T
+export type WatchCallback<V = any, OV = any> = (value: V, oldValue: OV) => void
 
-type OnCleanup = (cleanupFn: () => void) => void
+export interface WatchOptions<Immediate = boolean> {
+  immediate?: Immediate
+  deep?: boolean
+}
 
 export function watch<T>(
-  source: WatchSource<T>,
-  cb: (newValue: T, oldValue: T) => void,
+  source: WatchSource<T> | WatchSource[],
+  cb: WatchCallback,
+  option: WatchOptions = {},
 ) {
-  const getter = () => source()
-  let oldValue = getter()
+  let getter: () => any
+  let isMultiSource = false
+  if (isFunction(source)) {
+    getter = () => source()
+  } else if (isRef<T>(source)) {
+    getter = () => source.value
+  } else if (isArray(source)) {
+    isMultiSource = true
+    getter = () => source.map(s => (isRef(s) ? s.value : s))
+  } else {
+    if (isObject(source)) option.deep = true
+    getter = () => source
+  }
+
+  if (option.deep) {
+    const baseGetter = getter
+    getter = () => traverse(baseGetter())
+  }
+
+  let oldValue: T | T[]
   const job = () => {
-    const newValue = getter()
-    if (hasChanged(newValue, oldValue)) {
+    const newValue = effect.run()
+    if (
+      option.deep ||
+      (isMultiSource
+        ? (newValue as any[]).some((v, i) =>
+            hasChanged(v, (oldValue as T[])?.[i]),
+          )
+        : hasChanged(newValue, oldValue))
+    ) {
       cb(newValue, oldValue)
       oldValue = newValue
     }
   }
 
   const effect = new ReactiveEffect(getter, job)
-  effect.run()
+
+  // initial run
+  if (option.immediate) {
+    job()
+  } else {
+    oldValue = effect.run()
+  }
+}
+
+function traverse(value: unknown, seen?: Set<unknown>) {
+  if (!isObject(value)) return value
+
+  seen = seen || new Set()
+  if (seen.has(value)) {
+    return value
+  }
+  seen.add(value)
+  if (isRef(value)) {
+    traverse(value.value, seen)
+  } else if (isArray(value)) {
+    for (let i = 0; i < value.length; i++) {
+      traverse(value[i], seen)
+    }
+  } else if (isSet(value) || isMap(value)) {
+    value.forEach((v: any) => {
+      traverse(v, seen)
+    })
+  } else if (isPlainObject(value)) {
+    for (const key in value) {
+      traverse(value[key], seen)
+    }
+  }
+  return value
 }
--- packages/shared/general.ts
+++ packages/shared/general.ts
@@ -1,12 +1,17 @@
+export const isMap = (val: unknown): val is Map<any, any> =>
+  toTypeString(val) === '[object Map]'
+export const isSet = (val: unknown): val is Set<any> =>
+  toTypeString(val) === '[object Set]'
 export const isFunction = (val: unknown): val is Function =>
   typeof val === 'function'
 export const isArray = Array.isArray
 export const isString = (val: unknown): val is string => typeof val === 'string'
 export const isObject = (val: unknown): val is Record<any, any> =>
   val !== null && typeof val === 'object'
+export const isPlainObject = (val: unknown): val is object =>
+  toTypeString(val) === '[object Object]'
 
 const hasOwnProperty = Object.prototype.hasOwnProperty
-
 export const hasOwn = (
   val: object,
   key: string | symbol,
@@ -25,3 +30,7 @@ export const toHandlerKey = (str: string) => (str ? `on${capitalize(str)}` : ``)
 
 export const hasChanged = (value: any, oldValue: any): boolean =>
   !Object.is(value, oldValue)
+
+export const objectToString = Object.prototype.toString
+export const toTypeString = (value: unknown): string =>
+  objectToString.call(value)
